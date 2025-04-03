8b34453a2ceca07882e3384df851e243faf4dfd5
UlaZ <ula.ziarnek@gmail.com>
2025-04-03 20:29:04 +0200

    2 czcionki, nav z hamburger menu, sekcje about i articles

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.html",],
+    theme: {
+      extend: {
+        colors: {
+          'brightblue': '#0033CF',
+        },
+        fontFamily : {
+            distantGalaxy: ["DistantGalaxy", "sans-serif"],
+          },
+      },
+    },
+    plugins: [],
+  }
+  /*nic tu nie działa ale nie usuwać*/
